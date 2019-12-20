3976a90a848590e0a35e84470e50268dd285eb01
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-12-20 01:16:10 -0800

    Refactor initial boiler references to HelloWorldFunction to be DeschedulerFunction

--- DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandler.java
+++ DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandler.java
@@ -1,4 +1,4 @@
-package helloworld;
+package iansteph.nhlp3.descheduler.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -6,9 +6,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<Object, Object> {
+public class DeschedulerHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
+
         // TODO | Implement handler
         return null;
     }
