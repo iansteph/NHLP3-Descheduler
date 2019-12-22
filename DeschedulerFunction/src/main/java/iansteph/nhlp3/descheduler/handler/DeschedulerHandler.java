4690406549cd45ee80e97218eab7f0e9c13f1e52
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-12-22 10:14:13 -0800

    Add NhlPlayByPlayModel GitHub packages as dependency

--- DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandler.java
+++ DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandler.java
@@ -2,15 +2,25 @@ package iansteph.nhlp3.descheduler.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import iansteph.nhlp3.descheduler.model.event.PlayEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class DeschedulerHandler implements RequestHandler<Object, Object> {
+public class DeschedulerHandler implements RequestHandler<PlayEvent, Object> {
 
-    public Object handleRequest(final Object input, final Context context) {
+    private static final Logger logger = LogManager.getLogger(DeschedulerHandler.class);
 
-        // TODO | Implement handler
+    public Object handleRequest(final PlayEvent playEvent, final Context context) {
+        // List targets
+        // If there are targets:
+        //     Remove the target
+        //     Put event in SQS queue with delay
+        //     Complete
+        // Else:
+        //     Delete the Rule
         return null;
     }
 }
