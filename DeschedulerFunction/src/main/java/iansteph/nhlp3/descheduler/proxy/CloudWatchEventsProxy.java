50d98f86f3d3d0f9113db13a85d5ff9280594dd3
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-12-24 23:48:13 -0800

    Revert "Testing CodePipeline | Revert "Implement Descheduler logic""
    This reverts commit 1dd0b9a68650fb52ee0f2e013a0a140f0ff71c62.

---
+++ DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/proxy/CloudWatchEventsProxy.java
@@ -0,0 +1,87 @@
+package iansteph.nhlp3.descheduler.proxy;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.cloudwatchevents.CloudWatchEventsClient;
+import software.amazon.awssdk.services.cloudwatchevents.model.DeleteRuleRequest;
+import software.amazon.awssdk.services.cloudwatchevents.model.DeleteRuleResponse;
+import software.amazon.awssdk.services.cloudwatchevents.model.ListTargetsByRuleRequest;
+import software.amazon.awssdk.services.cloudwatchevents.model.ListTargetsByRuleResponse;
+import software.amazon.awssdk.services.cloudwatchevents.model.RemoveTargetsRequest;
+import software.amazon.awssdk.services.cloudwatchevents.model.RemoveTargetsResponse;
+import software.amazon.awssdk.services.cloudwatchevents.model.Target;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class CloudWatchEventsProxy {
+
+    private final CloudWatchEventsClient cloudWatchEventsClient;
+
+    private static final String TARGET_ID = "Event-Publisher-Lambda-Function";
+
+    private static final Logger logger = LogManager.getLogger(CloudWatchEventsProxy.class);
+
+    public CloudWatchEventsProxy(final CloudWatchEventsClient cloudWatchEventsClient) {
+
+        this.cloudWatchEventsClient = cloudWatchEventsClient;
+    }
+
+    public List<Target> listTargetsByRule(final String ruleName) {
+
+        try {
+
+            final ListTargetsByRuleRequest listTargetsByRuleRequest = ListTargetsByRuleRequest.builder()
+                    .rule(ruleName)
+                    .build();
+            logger.info(format("ListTargetsByRule request: %s", listTargetsByRuleRequest));
+            final ListTargetsByRuleResponse listTargetsByRuleResponse = cloudWatchEventsClient.listTargetsByRule(listTargetsByRuleRequest);
+            logger.info(format("ListTargetsByRule response: %s", listTargetsByRuleResponse));
+            return listTargetsByRuleResponse.targets();
+        }
+        catch (SdkException e) {
+
+            logger.error(e);
+            throw e;
+        }
+    }
+
+    public void deleteRule(final String ruleName) {
+
+        try {
+
+            final DeleteRuleRequest deleteRuleRequest = DeleteRuleRequest.builder()
+                    .name(ruleName)
+                    .build();
+            logger.info(format("DeleteRule request: %s", deleteRuleRequest));
+            final DeleteRuleResponse deleteRuleResponse = cloudWatchEventsClient.deleteRule(deleteRuleRequest);
+            logger.info(format("DeleteRule response: %s", deleteRuleResponse));
+        }
+        catch (SdkException e) {
+
+            logger.error(e);
+            throw e;
+        }
+    }
+
+    public void removeTargets(final String ruleName) {
+
+        try {
+
+            final RemoveTargetsRequest removeTargetsRequest = RemoveTargetsRequest.builder()
+                    .rule(ruleName)
+                    .ids(TARGET_ID)
+                    .build();
+            logger.info(format("RemoveTargets request: %s", removeTargetsRequest));
+            final RemoveTargetsResponse removeTargetsResponse = cloudWatchEventsClient.removeTargets(removeTargetsRequest);
+            logger.info(format("RemoveTargets response: %s", removeTargetsResponse));
+        }
+        catch (SdkException e) {
+
+            logger.error(e);
+            throw e;
+        }
+    }
+}
--- DeschedulerFunction/src/test/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandlerTest.java
+++ DeschedulerFunction/src/test/java/iansteph/nhlp3/descheduler/handler/DeschedulerHandlerTest.java
@@ -1,14 +1,129 @@
 package iansteph.nhlp3.descheduler.handler;
 
-import static org.junit.Assert.assertTrue;
+import iansteph.nhlp3.descheduler.model.event.PlayEvent;
+import iansteph.nhlp3.descheduler.proxy.CloudWatchEventsProxy;
+import iansteph.nhlp3.descheduler.proxy.SqsProxy;
 import org.junit.Test;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.cloudwatchevents.model.Target;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class DeschedulerHandlerTest {
 
-    @Test
-    public void successfulResponse() {
+    private final CloudWatchEventsProxy mockCloudWatchEventsProxy = mock(CloudWatchEventsProxy.class);
+    private final SqsProxy mockSqsProxy = mock(SqsProxy.class);
+    private final DeschedulerHandler deschedulerHandler = new DeschedulerHandler(mockCloudWatchEventsProxy, mockSqsProxy);
 
-        // TODO | Stub test. Implementation TBD, but passes so build completes
-        assertTrue(true);
+    @Test(expected = NullPointerException.class)
+    public void test_handleRequest_throws_NullPointerException_because_input_play_event_is_null() {
+
+        deschedulerHandler.handleRequest(null, null);
+
+        verify(mockCloudWatchEventsProxy, never()).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, never()).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+    }
+
+    @Test(expected = SdkException.class)
+    public void test_handleRequest_throws_SdkException_because_listTargetsByRule_call_to_CloudWatchEventsProxy_fails() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenThrow(SdkException.builder().build());
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, never()).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+    }
+
+    @Test
+    public void test_handleRequest_successfully_handles_request_without_targets() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.emptyList());
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, never()).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).deleteRule(anyString());
+    }
+
+    @Test(expected = SdkException.class)
+    public void test_handleRequest_throws_SdkException_when_handling_request_without_targets_because_deleteRule_call_to_CloudWatchEventsProxy_fails() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.emptyList());
+        doThrow(SdkException.builder().build()).when(mockCloudWatchEventsProxy).deleteRule(anyString());
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, never()).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).deleteRule(anyString());
+    }
+
+    @Test
+    public void test_handleRequest_successfully_handles_request_with_remaining_targets() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.singletonList(Target.builder().build()));
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+        verify(mockSqsProxy, times(1)).sendMessage(any(PlayEvent.class));
+    }
+
+    @Test(expected = SdkException.class)
+    public void test_handleRequest_throws_SdkException_when_handling_request_with_remaining_targets_because_removeTargets_call_to_CloudWatchEventsProxy_fails() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.singletonList(Target.builder().build()));
+        doThrow(SdkException.builder().build()).when(mockCloudWatchEventsProxy).removeTargets(anyString());
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+        verify(mockSqsProxy, never()).sendMessage(any(PlayEvent.class));
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test_handleRequest_throws_RuntimeException_when_handling_request_with_remaining_targets_because_sendMessage_call_to_SqsProxy_fails() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.singletonList(Target.builder().build()));
+        doThrow(new RuntimeException()).when(mockSqsProxy).sendMessage(any(PlayEvent.class));
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+        verify(mockSqsProxy, times(1)).sendMessage(any(PlayEvent.class));
+    }
+
+    @Test(expected = SdkException.class)
+    public void test_handleRequest_throws_SdkException_when_handling_request_with_remaining_targets_because_sendMessage_call_to_SqsProxy_fails() {
+
+        when(mockCloudWatchEventsProxy.listTargetsByRule(anyString())).thenReturn(Collections.singletonList(Target.builder().build()));
+        doThrow(SdkException.builder().build()).when(mockSqsProxy).sendMessage(any(PlayEvent.class));
+
+        deschedulerHandler.handleRequest(new PlayEvent(), null);
+
+        verify(mockCloudWatchEventsProxy, times(1)).listTargetsByRule(anyString());
+        verify(mockCloudWatchEventsProxy, times(1)).removeTargets(anyString());
+        verify(mockCloudWatchEventsProxy, never()).deleteRule(anyString());
+        verify(mockSqsProxy, times(1)).sendMessage(any(PlayEvent.class));
     }
 }
