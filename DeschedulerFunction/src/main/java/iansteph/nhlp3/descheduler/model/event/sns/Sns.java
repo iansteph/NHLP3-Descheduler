a1b2d70e66b3588b9b53960bd82f8efafc137c8e
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2020-02-11 23:16:09 -0800

    ✂️ Remove unused fields from Sns model
    - Remove MessageId
    - Remove Timestamp
    - Remove MessageAttributes

--- DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/model/event/sns/Sns.java
+++ DeschedulerFunction/src/main/java/iansteph/nhlp3/descheduler/model/event/sns/Sns.java
@@ -6,43 +6,14 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import iansteph.nhlp3.descheduler.deserialization.PlayEventDeserializer;
 import iansteph.nhlp3.descheduler.model.event.PlayEvent;
 
-import java.util.Map;
 import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Sns {
 
-    @JsonProperty(value = "Timestamp")
-    private String timestamp;
-
-    @JsonProperty(value = "MessageId")
-    private String messageId;
-
     @JsonProperty(value = "Message")
     @JsonDeserialize(using = PlayEventDeserializer.class)
     private PlayEvent message;
-
-    private Map<String, MessageAttribute> messageAttributes;
-
-    public String getTimestamp() {
-
-        return timestamp;
-    }
-
-    public void setTimestamp(final String timestamp) {
-
-        this.timestamp = timestamp;
-    }
-
-    public String getMessageId() {
-
-        return messageId;
-    }
-
-    public void setMessageId(final String messageId) {
-
-        this.messageId = messageId;
-    }
 
     public PlayEvent getMessage() {
 
@@ -54,24 +25,11 @@ public class Sns {
         this.message = message;
     }
 
-    public Map<String, MessageAttribute> getMessageAttributes() {
-
-        return messageAttributes;
-    }
-
-    public void setMessageAttributes(final Map<String, MessageAttribute> messageAttributes) {
-
-        this.messageAttributes = messageAttributes;
-    }
-
     @Override
     public String toString() {
 
         return "Sns{" +
-                "timestamp='" + timestamp + '\'' +
-                ", messageId='" + messageId + '\'' +
-                ", message=" + message +
-                ", messageAttributes=" + messageAttributes +
+                "message=" + message +
                 '}';
     }
 
@@ -81,15 +39,12 @@ public class Sns {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sns sns = (Sns) o;
-        return Objects.equals(timestamp, sns.timestamp) &&
-                Objects.equals(messageId, sns.messageId) &&
-                Objects.equals(message, sns.message) &&
-                Objects.equals(messageAttributes, sns.messageAttributes);
+        return Objects.equals(message, sns.message);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(timestamp, messageId, message, messageAttributes);
+        return Objects.hash(message);
     }
 }
